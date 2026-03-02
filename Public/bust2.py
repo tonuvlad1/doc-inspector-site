1e46d7eaec20230bbd86de0df6c43b35c8fd14b9
tonuvlad1 <48450812+tonuvlad1@users.noreply.github.com>
2026-03-02 13:23:41 +0200

    Add files via upload

---
+++ Public/bust2.py
@@ -0,0 +1,14 @@
+import glob
+
+html_files = glob.glob('c:/Users/Admin/Desktop/doc-inspector-site-main/Public/*.html')
+
+for filepath in html_files:
+    with open(filepath, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    # Simple cache bust parameter
+    content = content.replace('href="style.css?v=3"', 'href="style.css?v=4"')
+    
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write(content)
+    print("Done", filepath)
